897f145ec46140db50b20f6811d294b59125ca55
MorganTHobson <mhobson5@jhu.edu>
2018-02-24 00:47:43 -0500

    fixed plotting for csv

--- plot.py
+++ plot.py
@@ -1,11 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-x=range(1,41)
-values=np.random.uniform(size=40)
+df = pd.read_csv("serial-data.csv", header=0)
 
-plt.stem(x, values)
-plt.ylim(0, 1.2)
+val = dict()
+
+for index, row in df.iterrows():
+  if not row['filter length'] in val:
+    val[row['filter length']] = [float(row['sec']) + float(row['us'])/1000000]
+  else:
+    val[row['filter length']].append(float(row['sec']) + float(row['us'])/1000000)
+
+x = []
+y = []
+
+for key,value in val.iteritems():
+  x.append(int(key))
+  y.append(np.median(value))
+
+plt.stem(x, y)
 
 plt.title('Title')
 plt.xlabel('X Values')
