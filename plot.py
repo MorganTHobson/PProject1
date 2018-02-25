13e2a3e92e860d517ccab6c32a24fc926bcd5710
MorganTHobson <mhobson5@jhu.edu>
2018-02-25 00:25:27 -0500

    added speedup graphs

--- plot.py
+++ plot.py
@@ -2,28 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("serial-data.csv", header=0)
+sdf = pd.read_csv("serial-filter.csv", header=0)
+pdf = pd.read_csv("parallel-filter.csv", header=0)
 
-val = dict()
+sval = dict()
+pval = dict()
 
-for index, row in df.iterrows():
-  if not row['filter length'] in val:
-    val[row['filter length']] = [float(row['sec']) + float(row['us'])/1000000]
+for index, row in sdf.iterrows():
+  if not row['filter length'] in sval:
+    sval[row['filter length']] = [float(row['sec']) + float(row['us'])/1000000]
   else:
-    val[row['filter length']].append(float(row['sec']) + float(row['us'])/1000000)
+    sval[row['filter length']].append(float(row['sec']) + float(row['us'])/1000000)
+
+serial_time = np.median(sval[512])
+
+for index, row in pdf.iterrows():
+  if not row['threads'] in pval:
+    pval[row['threads']] = [float(row['sec']) + float(row['us'])/1000000]
+  else:
+    pval[row['threads']].append(float(row['sec']) + float(row['us'])/1000000)
 
 x = []
 y = []
 
-for key,value in val.iteritems():
+for key,value in pval.iteritems():
   x.append(int(key))
-  y.append(512*512*256 * int(key) / np.median(value))
+  y.append(serial_time/np.median(value))
 
 plt.stem(x,y)
 
-plt.title('Normalized Serial Data First')
-plt.xlabel('Filter Length')
-plt.ylabel('Filter Length * Data Length / Time (op/sec)')
+plt.title('Parallel Filter First Speedup')
+plt.xlabel('Threads')
+plt.ylabel('Serial Time / Parallel Time')
 
 #plt.xscale('log')
 
